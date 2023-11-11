956fa7fa5a6c2719e8250c2fa9ef11a1e740ae7a
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-11-11 14:47:40 +0900

    イベントバインディングをお試し

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -3,13 +3,22 @@ defineProps({
   text: String
 })
 const dataTest = 'これはデータです'
-const imageUrl = 'https://source.unsplash.com/random/800x800'
+const imageUrl = 'https://source.unsplash.com/random/200x200'
+const myMethod = () => {
+  alert('メソッド実行')
+}
 </script>
 
 <template>
   <p>{{ dataTest }}</p>
+
   <p v-text="dataTest"></p>
+
   <img v-bind:src="imageUrl" />
   <!-- 省略した書き方 -->
   <img :src="imageUrl" />
+
+  <button v-on:click="myMethod">メソッド実行</button>
+  <!-- 省略した書き方 -->
+  <button @click="myMethod">メソッド実行</button>
 </template>
