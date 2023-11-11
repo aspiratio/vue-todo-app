5c297e4554da480f7731dc4a3bfae4a7b28e747a
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-11-11 14:42:03 +0900

    属性バインディングをお試し

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -3,8 +3,13 @@ defineProps({
   text: String
 })
 const dataTest = 'これはデータです'
+const imageUrl = 'https://source.unsplash.com/random/800x800'
 </script>
 
 <template>
+  <p>{{ dataTest }}</p>
   <p v-text="dataTest"></p>
+  <img v-bind:src="imageUrl" />
+  <!-- 省略した書き方 -->
+  <img :src="imageUrl" />
 </template>
